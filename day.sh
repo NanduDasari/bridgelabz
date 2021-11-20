7b77a900781c2887f1b0eab51f70b43bbfd86024
nandini <dasari.nandini5858@gmail.com>
2021-11-20 16:57:20 +0530

    it contains the case statements

---
+++ day.sh
@@ -0,0 +1,18 @@
+day=15
+case $day in
+5)
+   for i in {1..5}
+do
+     echo $i
+done
+    ;;
+10)
+    for i in {1..10}
+do
+      echo $i
+done
+    ;;
+*)
+     echo "none of the above"
+    ;;
+esac
