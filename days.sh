d1eea22b5a14a8c97a7d2fe4322118e0e0a6511f
nandini <dasari.nandini5858@gmail.com>
2021-11-20 15:53:51 +0530

    it perform the days by using nested if statement

---
+++ days.sh
@@ -0,0 +1,13 @@
+day="thursday"
+if [ $day = "sunday" ]
+then
+        echo "sunday"
+elif [ $day = "monday" ]
+then
+        echo "monday"
+elif [ $day = "friday" ]
+then
+        echo "friday"
+else
+echo "none of the above"
+fi
